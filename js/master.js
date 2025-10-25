b8e339b64b4159b5eae4beccf94ea0074073733d
ahmed ibrahem <ahmedalfar945@gmail.com>
2025-10-25 18:38:03 +0300

    add alternative photos and some edits in code

--- js/master.js
+++ js/master.js
@@ -1,6 +1,17 @@
-// let landingPage = document.querySelector(".landing-page");
-// let imgsArray = ["1.jpg","2.jfif","3.jpg","4.jpg","5.jpeg"];
-// setInterval(() => {
-//     let randomNumber = Math.floor(Math.random() * imgsArray.length);
-//     landingPage.style.backgroundImage = 'url("images/' + imgsArray[randomNumber] + '")';
-// },10000);
+let landingPage = document.querySelector(".landing-page");
+let imgsArray = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+setInterval(() => {
+    let randomNumber = Math.floor(Math.random() * imgsArray.length);
+    landingPage.style.backgroundImage = 'url("images/' + imgsArray[randomNumber] + '")';
+},10000);
+
+
+let listButton = document.querySelector('.list');
+let list = document.querySelector('nav');
+
+listButton.addEventListener("click", ()=>{
+    list.style.visibility = "visible";
+});
+listButton.addEventListener('blur', ()=>{
+    list.style.visibility = "hidden";
+})
